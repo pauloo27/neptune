83213794ab24cbd9a6f70ba4f4af540c352239e5
Paulo <pauloeduardo2709@gmail.com>
2020-11-22 16:14:10 -0300

    Add search bar, reduce spacing

--- gui/app/main.go
+++ gui/app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	boxSpacing = 5
+	boxSpacing = 1
 )
 
 var appWin *gtk.Window
@@ -24,14 +24,37 @@ func Start() {
 
 	appWin = win
 
+	baseContainer, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, boxSpacing)
+
+	// search bar
+	searchBarContainer, err := gtk.HeaderBarNew()
+	utils.HandleError(err, "Cannot create header bar")
+
+	searchBarContainer.SetShowCloseButton(false)
+
+	searchInput, err := gtk.EntryNew()
+	utils.HandleError(err, "Cannot create entry")
+
+	searchInput.SetPlaceholderText("Search YouTube")
+	searchInput.SetHExpand(true)
+
+	searchButton, err := gtk.ButtonNewFromIconName("search", gtk.ICON_SIZE_BUTTON)
+
+	searchBarContainer.PackStart(searchInput)
+	searchBarContainer.PackStart(searchButton)
+
+	// main content container
 	mainContainer, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, boxSpacing)
 	utils.HandleError(err, "Cannot create box")
-
-	win.Add(mainContainer)
 
 	mainContainer.SetHomogeneous(true)
 	mainContainer.PackStart(CreatePlayer(), false, true, boxSpacing)
 	mainContainer.PackEnd(CreateLibrary(), false, true, boxSpacing)
+
+	win.Add(baseContainer)
+
+	baseContainer.PackStart(searchBarContainer, false, false, boxSpacing)
+	baseContainer.PackEnd(mainContainer, true, true, boxSpacing)
 
 	win.SetDefaultSize(800, 600)
 
