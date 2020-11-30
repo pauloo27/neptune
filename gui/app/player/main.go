32cf9d959733573a66a5a8a396c3835cd72e5caa
Paulo <pauloeduardo2709@gmail.com>
2020-11-30 18:30:30 -0300

    Add album art to player

--- gui/app/player/main.go
+++ gui/app/player/main.go
@@ -9,6 +9,7 @@ func CreatePlayer() *gtk.Box {
 	playerContainer, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
 	utils.HandleError(err, "Cannot create box")
 
+	playerContainer.PackStart(createPlayerTop(), true, true, 0)
 	playerContainer.PackEnd(createPlayerBottom(), false, false, 0)
 
 	return playerContainer
---
+++ gui/app/player/top.go
@@ -0,0 +1,33 @@
+package player
+
+import (
+	"github.com/Pauloo27/neptune/player"
+	"github.com/Pauloo27/neptune/utils"
+	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func createPlayerTop() *gtk.Box {
+	topContainer, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	utils.HandleError(err, "Cannot create box")
+
+	// album art
+	albumArt, err := gtk.ImageNew()
+	utils.HandleError(err, "Cannot create image")
+
+	albumArt.SetVAlign(gtk.ALIGN_CENTER)
+
+	player.RegisterHook(player.HOOK_FILE_LOADED, func(params ...interface{}) {
+		imagePath := player.State.Track.Album.GetAlbumArtPath()
+		glib.IdleAdd(func() {
+			imagePix, err := gdk.PixbufNewFromFileAtScale(imagePath, 300, 300, true)
+			utils.HandleError(err, "Cannot load image from file")
+			albumArt.SetFromPixbuf(imagePix)
+		})
+	})
+
+	topContainer.PackStart(albumArt, true, true, 1)
+
+	return topContainer
+}
