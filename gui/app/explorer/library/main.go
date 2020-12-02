0194f43a99e62751605fcf94f9504e4e539a4f86
Paulo <pauloeduardo2709@gmail.com>
2020-12-02 19:32:14 -0300

    Add header

--- gui/app/explorer/library/main.go
+++ gui/app/explorer/library/main.go
@@ -5,9 +5,16 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+var titleHeader *gtk.Label
+
 func CreateLibraryPage() *gtk.Box {
 	libraryContainer, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
 	utils.HandleError(err, "Cannot create box")
+
+	titleHeaader, err := gtk.LabelNew("Home")
+	utils.HandleError(err, "Cannot create label")
+
+	libraryContainer.PackStart(titleHeaader, false, false, 0)
 
 	return libraryContainer
 }
